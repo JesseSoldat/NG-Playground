2c1cc068d800433bedc5f35cad7120e2732a09c3
JesseSoldat <jsoldat@hotmail.com>
2018-01-10 12:33:40 +0700

    routing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import '../shared/rxjs-operators';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     BrowserAnimationsModule,
   ],
   exports: [ 
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HeaderComponent
   ],
   providers: [
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+
+const dashboardRoutes: Routes = [
+  { path: '', component: DashboardComponent }
+]
 
 @NgModule({
   declarations: [
     DashboardComponent
   ],
-  imports: []
+  imports: [ RouterModule.forChild(dashboardRoutes) ],
+  exports: [ RouterModule ]
 })
 export class DashboardModule {}
